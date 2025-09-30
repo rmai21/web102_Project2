d047ca731a698f432b1686ea9eb476b1abd3a408
rmai21 <50464351+rmai21@users.noreply.github.com>
2025-09-30 17:18:45 -0400

    Added colors, and flip feature

--- src/Flashcard.jsx
+++ src/Flashcard.jsx
@@ -3,18 +3,28 @@ import React, { useState } from "react";
 
 const Flashcard = ({ flashcards }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [showAnswer, setShowAnswer] = useState(false);
 
-  const nextCard = () =>
+  const nextCard = () => {
     setCurrentIndex((prev) =>
       prev < flashcards.length - 1 ? prev + 1 : prev
     );
-  const prevCard = () =>
+    setShowAnswer(false);
+  };
+
+  const prevCard = () => {
     setCurrentIndex((prev) => (prev > 0 ? prev - 1 : prev));
+    setShowAnswer(false); 
+  };
+
+  const flipCard = () => {
+    setShowAnswer((prev) => !prev); 
+  };
 
   return (
     <div>
-      <div className="card">
-        <h2>{flashcards[currentIndex].question}</h2>
+      <div className="card" onClick={flipCard}>
+        <h2>{showAnswer ? flashcards[currentIndex].answer : flashcards[currentIndex].question}</h2>
       </div>
 
       <div className="navigation-buttons">
